1895a0df42905b57d9cbde2e4c776613251a92e7
Muhammad Ali <mohammad_ali16@yahoo.com>
2020-06-25 22:26:43 +0500

    ali commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            UserDBHandle dbhandle = new UserDBHandle();
+            ModelState.Clear();
+            return View(dbhandle.GetLogins());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    UserDBHandle db = new UserDBHandle();
+                    if (db.AddUser(login))
+                    {
+                        ViewBag.Message = "User Details Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            UserDBHandle db = new UserDBHandle();
+            return View(db.GetLogins().Find(smodel => smodel.ID == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Employee login)
+        {
+            try
+            {
+                //login.ID = id;
+                UserDBHandle db = new UserDBHandle();
+                db.UpdateDetails(login);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                UserDBHandle db = new UserDBHandle();
+                if (db.DeleteUser(id))
+                {
+                    ViewBag.AlertMsg = "User Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/UserDBHandle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace Project.Models
+{
+    public class UserDBHandle
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            //string constring = @"Data Source=HAIER-pc;Initial Catalog=OOSEPROJECT;Integrated Security=True";
+            string constring = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            con = new SqlConnection(constring); 
+        }
+        public bool AddUser(Employee login)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("AddNewUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@UserName", login.UserName);
+            cmd.Parameters.AddWithValue("@UserPassword", login.UserPassword);
+            cmd.Parameters.AddWithValue("@type", login.UserType);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+      
+        public List<Employee> GetLogins()
+        {
+            connection();
+            List <Employee> UserList = new List<Employee>();
+            SqlCommand cmd = new SqlCommand("GetUserDetails", con);
+            cmd.CommandType =CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+            foreach (DataRow dr in dt.Rows)
+            {
+                UserList.Add(
+                    new Employee
+                    {
+                        ID = Convert.ToInt32(dr["UserID"]),
+                        UserName = Convert.ToString(dr["UserName"]),
+                        UserPassword = Convert.ToString(dr["UserPassword"]),
+                        UserType = Convert.ToString(dr["type"])
+                    }
+                    );
+            }
+            return UserList;
+        }
+        public bool UpdateDetails(Employee login)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("UpdateUserDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+ 
+            cmd.Parameters.AddWithValue("@UserID", login.ID);
+            cmd.Parameters.AddWithValue("@UserName", login.UserName);
+            cmd.Parameters.AddWithValue("@UserPassword", login.UserPassword);
+            cmd.Parameters.AddWithValue("@type", login.UserType);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+ 
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+        public bool DeleteUser(int id)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("DeleteUser", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+ 
+            cmd.Parameters.AddWithValue("@UserID", id);
+ 
+            con.Open();
+	            int i = cmd.ExecuteNonQuery();
+	            con.Close();
+	 
+	            if (i >= 1)
+	                return true;
+	            else
+	                return false;
+	    }
+	}
+}
