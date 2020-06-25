59243ffcf4886e117606c2ee268f9e47206e2495
khamisani_99 <skhamisani@yahoo.com>
2020-06-25 23:40:08 +0500

    Revert "File sync"
    This reverts commit 23fe596b5dffaf84774c6e51bb384ab27af78fd6.

---
+++ Controllers/MedicinController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class MedicinController : Controller
+    {
+        // GET: Medicin
+        public ActionResult Index()
+        {
+            MedicineDB dbhandle = new MedicineDB();
+            ModelState.Clear();
+            return View(dbhandle.GetLogins());
+        }
+        public ActionResult IndexC()
+        {
+            MedicineDB dbhandle = new MedicineDB();
+            ModelState.Clear();
+            return View(dbhandle.GetLogins());
+        }
+        public ActionResult IndexE()
+        {
+            MedicineDB dbhandle = new MedicineDB();
+            ModelState.Clear();
+            return View(dbhandle.GetLogins());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Medicine login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MedicineDB db = new MedicineDB();
+                    if (db.AddMedicine(login))
+                    {
+                        ViewBag.Message = "Medicine Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            MedicineDB db = new MedicineDB();
+            return View(db.GetLogins().Find(smodel => smodel.DrugID == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, Medicine login)
+        {
+            try
+            {
+                MedicineDB db = new MedicineDB();
+                db.UpdateDetails(login);
+                return RedirectToAction("Index");
+            }
+            catch(Exception e)
+            {
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                MedicineDB db = new MedicineDB();
+                if (db.DeleteUser(id))
+                {
+                    ViewBag.AlertMsg = "Medicine Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+    }
+}
